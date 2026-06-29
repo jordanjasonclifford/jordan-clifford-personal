e13b3f406a39c82ee08892fbe213229c82f973b1
jordanjasonclifford <jordanjasonclifford@gmail.com>
2026-06-28 20:09:04 -0700

    more little changes

--- src/comps/Footer.jsx
+++ src/comps/Footer.jsx
@@ -1,10 +1,10 @@
 export default function Footer() {
   return (
-    <footer className="py-8 bg-black border-t border-orange-300/10 text-center text-sm text-white">
+    <footer className="py-10 bg-black border-t border-orange-300/10 text-center text-base text-white">
       <img
         src="/jordancliffordwebsitelogo.png"
         alt="Jordan Clifford logo"
-        className="mx-auto mb-4 h-24 w-auto opacity-95"
+        className="mx-auto mb-5 h-32 w-auto opacity-95"
       />
 
       <p className="mb-1">
@@ -18,7 +18,7 @@ export default function Footer() {
 
       <p className="mb-1">&copy; 2026 Jordan Clifford.</p>
 
-      <p className="text-xs text-white">
+      <p className="text-sm text-white">
         Hero background video courtesy of Pixabay (creator: FlickrVideos)
       </p>
     </footer>
