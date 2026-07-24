207462b98b225272df651bb06e756030c9c74b60
jordanjasonclifford <jordanjasonclifford@gmail.com>
2026-07-23 19:42:18 -0700

    More pronounced boarders

--- src/comps/MesaDivider.jsx
+++ src/comps/MesaDivider.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function MesaDivider({ from = "#5c2d14", to = "#6a3422" }) {
   return (
     <div
-      className="h-10 overflow-hidden leading-none"
+      className="h-8 overflow-hidden leading-none sm:h-10 md:h-14 lg:h-16"
       style={{ backgroundColor: from }}
       aria-hidden="true"
     >
@@ -11,11 +11,11 @@ export default function MesaDivider({ from = "#5c2d14", to = "#6a3422" }) {
         viewBox="0 0 100 40"
         preserveAspectRatio="none"
         shapeRendering="crispEdges"
-        className="block h-10 w-full"
+        className="block h-full w-full"
       >
         <path
           fill={to}
-          d="M0 13H8V17H18V9H31V15H43V6H55V14H67V10H79V18H91V12H100V40H0Z"
+          d="M0 9H4V15H10V21H16V13H22V7H29V17H36V23H43V12H50V6H57V16H64V20H71V11H78V5H85V15H92V22H97V13H100V40H0Z"
         />
       </svg>
     </div>
